93a07d16eefebee006ec48f757305b3b8839e5a7
ekkamon <ekkb.contact@gmail.com>
2023-06-02 12:37:05 +0700

    [change] glob path (getImageUrl)

--- src/utils/misc.js
+++ src/utils/misc.js
@@ -1,7 +1,7 @@
 export const isEnvBrowser = () => !window.invokeNative;
 
 export const getImageUrl = (path) => {
-  const modules = import.meta.globEager('../assets/images/**/*.png');
+  const modules = import.meta.globEager('../assets/images/**/*');
 
   return modules['../assets/images/' + path]?.default;
 };
